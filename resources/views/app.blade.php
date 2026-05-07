20bd0d8d5ef59b005ec2f57b82def5a10712e987
Fabian Alba <fabianalbamanzano14@gmail.com>
2026-05-07 14:48:05 +0200

    Cambiamos el titulo de la web

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <title inertia>Fabián Alba - Fullstack Developer</title>
 
         <!-- Fonts -->
         <link scroll-behavior: smoothpreconnect" href="https://fonts.bunny.net">
